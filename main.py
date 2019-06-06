bdf833436fbd9147c0cc5247ffcbc52e99e37f69
Ainaz Hajimoradlou <ainaz@cs.ubc.ca>
2019-06-06 16:24:52 -0700

    created a branch for the previous version

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ from pathlib import Path
 from random import shuffle
 from torch.autograd import Variable
 
+def is_eq(pred_indices, label_vec):
+    for idx in pred_indices:
+        if label_vec[idx] == 1:
+            return 1
+    return 0
+
 attr_lables = {
     '/m/02gy9n': 0, #Transparent
     '/m/05z87': 1, #Plastic
@@ -26,9 +32,9 @@ q = list(p.glob('*.jpg'))
 img_id, id_bbox_dict, id_labels = preprocess.prepare_data(attr_lables)
 print("1. prepared data - check")
 
-# find_feature.find_features(id_bbox_dict, id_labels, q) # saving features for all images (with 'is' relation and not corrupt)
-# print('2. saved features - check')
-print('features are already saved - check')
+find_feature.find_features(id_bbox_dict, id_labels, q) # saving features for all images (with 'is' relation and not corrupt)
+print('2. saved features - check')
+# print('features are already saved - check')
 
 feature_path = Path('bbox_features/')
 feature_path_list = list(feature_path.glob('*.npy'))
@@ -53,13 +59,29 @@ print(len(val_feature_path))
 
 print('3. separated train and validation - check')
 
-attr_classifier = model.MultiLabelClassifier(100, 40, 5).cuda()
+attr_classifier = model.MultiLabelClassifier(300, 40, 5).cuda()
 # attr_classifier = model.MultiLabelClassifier(300, 40, 5)
 attr_classifier.eval()
-print('4. model created - check')
+print("4. model created - check")
 
-train_accuracy, val_accuracy = train_iters.train_iters(attr_classifier, train_feature_path, val_feature_path)
-print('5. train completed - check')
+train_iters.train_iters(attr_classifier, train_feature_path, val_feature_path)
+# print("5. train completed - check")
 
-print('6. calculating accuracy - check')
-print('--- train: %f, validation: %f' %(train_accuracy, val_accuracy))
+# total_accuracy = 0
+# softmax = nn.Softmax()
+# cnt = 0
+
+# for i in range(len(val_feature_path_idx)):
+#     feature_vec, label_vec = train_iters.load_features_labels([feature_path_list[val_feature_path_idx[i]]], id_labels)
+    
+#     for j in range(len(feature_vec)):
+#         x = Variable(torch.FloatTensor(np.array(feature_vec)[j])).cuda()
+#         # x = Variable(torch.FloatTensor(np.array(feature_vec)[j]))
+#         y_pred = attr_classifier(x)
+#         label_likelihood = softmax(y_pred).squeeze()
+#         probs, indices = label_likelihood.topk(1)
+#         total_accuracy = total_accuracy + is_eq(indices, label_vec[j])
+#         del x, y_pred
+#     cnt = cnt + len(feature_vec)
+
+# print("------- accuracy: %f" %(total_accuracy / cnt))
--- train_iters.py
+++ train_iters.py
@@ -5,12 +5,12 @@ import torch.nn as nn
 import torch.optim
 import numpy as np
 import torch
-import random
 
 writer = SummaryWriter()
 
 def load_features_labels(path_names):
-    feature_vec = []
+    feature_vec = [] # feature vector is a vector whose element has all the related bboxes for an image,
+                     # the name of the image is the same as the image id
     label_vec = []
     for i in range(len(path_names)):
         f = np.load(str(path_names[i])) # f has two vectors, one of size 4096 and one of size 5
@@ -18,87 +18,103 @@ def load_features_labels(path_names):
         feature_vec.append(f[0].reshape(1, 4096))
         label_vec.append(f[1].reshape(1, 5))
         # id = str(path_names[i])[-20:-4]
-    return feature_vec, label_vec
+        
+        # for idx, feature in enumerate(f):
+            # norm = np.linalg.norm(feature, 2)
+            # feature_vec.append(feature/norm)
+#            print(feature_vec[-1])
+            # label = np.zeros(5)
+            # label[id_labels[id][idx]] = 1 # id_labels[id] returns a list of ids corresponding to bboxes
+            # label_vec.append(label)
 
-def validate(model, val_feature_vec, val_label_vec):
+    return feature_vec, label_vec        
+
+def validate(model, val_feature_path_idx, feature_path_list, id_labels):
     criterion = nn.MultiLabelSoftMarginLoss()
-    # num_iters = 3
-    # batch_size = len(val_feature_vec)//num_iters
-    # avg_loss = 0
+    num_iters = 3
+    batch_size = len(val_feature_path_idx)//num_iters
+    avg_loss = 0
 
-    # for i in range(num_iters):
-    val_x = Variable(torch.FloatTensor(np.array(val_feature_vec))).cuda()
-    val_y = Variable(torch.FloatTensor(np.array(val_label_vec))).cuda()
-    val_y_pred = model(val_x)
-    loss = criterion(val_y_pred, val_y)
-    # avg_loss += float(loss)
-        # del val_x, val_y, val_y_pred
-    return float(loss)
+    for i in range(num_iters):
+        start_idx = i * batch_size  % len(val_feature_path_idx)
+        if start_idx+batch_size > len(val_feature_path_idx):
+            continue
 
-def is_eq(pred_indices, label_vec):
-    for idx in pred_indices:
-        if label_vec[idx] == 1:
-            return 1
-    return 0
+        val_feature_path = []
+        for e in val_feature_path_idx[start_idx:start_idx+batch_size]:
+            val_feature_path.append(feature_path_list[e])
+        
+        feature_vec, label_vec = load_features_labels(val_feature_path, id_labels)
+        
+        val_x = Variable(torch.FloatTensor(np.array(feature_vec))).cuda()
+        # val_x = Variable(torch.FloatTensor(np.array(feature_vec)))
+        val_y = Variable(torch.FloatTensor(np.array(label_vec))).cuda()
+        # val_y = Variable(torch.FloatTensor(np.array(label_vec)))
+        val_y_pred = model(val_x)
+        loss = criterion(val_y_pred, val_y)
+        avg_loss = avg_loss + loss.item()
 
-def find_accuracy(model, feature_vec, label_vec):
-    total_accuracy = 0
-    softmax = nn.Softmax()
-    for i in range(len(feature_vec)):
-        x = Variable(torch.FloatTensor(feature_vec[i])).cuda()
-        y_pred = model(x)
-        label_likelihood = softmax(y_pred).squeeze()
-        probs, indices = label_likelihood.topk(1)
-        total_accuracy = total_accuracy + is_eq(indices, label_vec[i][0])
-
-    return total_accuracy/len(feature_vec)
-
-def train_iters(model, train_feature_path, val_feature_path, learning_rate=0.01, batch_size=20, epochs=50):
+        #del val_x, val_y, val_y_pred
+    
+    return avg_loss/num_iters
+        
+def train_iters(model, train_feature_path, val_feature_path, learning_rate=0.001, batch_size=20, epochs=2):
     criterion = nn.MultiLabelSoftMarginLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-5)
     # scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, 0.95)
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min')
+    scheduler = torch.optim.ReduceLROnPlateau(optimizer, 'min')
 
-    train_loss, val_loss, avg_loss = 0, 0, 0
-    train_accuracy, val_accuracy = 0, 0
-    num_iter = len(train_feature_path)//batch_size
-
+    train_losses = []
+    val_losses = []
+    num_iter = len(train_feature_path_idx)//batch_size
+    #print(len(train_feature_path_idx))
+    #print(num_iter)
+    avg_loss = 0
     train_feature_vec, train_label_vec = load_features_labels(train_feature_path)
     val_feature_vec, val_label_vec = load_features_labels(val_feature_path)
-    print('*** loaded train and val features')
 
     for i in range(epochs):
         for j in range(num_iter):
-            x = Variable(torch.FloatTensor(np.array(train_feature_vec[j*batch_size:(j+1)*batch_size]))).cuda()
-            y = Variable(torch.FloatTensor(np.array(train_label_vec[j*batch_size:(j+1)*batch_size]))).cuda()
+            # start_idx = j * batch_size % len(train_feature_path_idx)
+            # if start_idx+batch_size > len(train_feature_path_idx):
+            #     continue
+            
+            # train_feature_path = []
+            # for idx in train_feature_path_idx[start_idx:start_idx+batch_size]:
+                # train_feature_path.append(feature_path_list[idx])
 
+            # feature_vec, label_vec = load_features_labels(train_feature_path, id_labels)
+            x = Variable(torch.FloatTensor(np.array(train_feature_vec[j*batch_size:(j+1)*batch_size]))).cuda()
+            # x = Variable(torch.FloatTensor(np.array(feature_vec)))
+            y = Variable(torch.FloatTensor(np.array(train_label_vec[j*batch_size:(j+1)*batch_size]))).cuda()
+            # y = Variable(torch.FloatTensor(np.array(label_vec)))
+            
             optimizer.zero_grad()
             y_pred = model(x)
             loss = criterion(y_pred, y)
-            avg_loss += float(loss)
-
+            
+            # writer.add_scalar('loss/train_every_iter', loss.data[0], i)
+            # loss.backward()
+            # optimizer.step()
+            # avg_loss += float(loss)
+            
+            if (i+1) % 1000 == 0:
+                train_losses.append(avg_loss / 1000)
+                avg_loss = 0
+                val_losses.append(validate(model, val_feature_path_idx, feature_path_list, id_labels))
+                print(i, train_losses[-1], val_losses[-1])
+                writer.add_scalar('loss/train', train_losses[-1], i)
+                writer.add_scalar('loss/validate', val_losses[-1], i)
+            
             loss.backward()
             optimizer.step()
+            avg_loss += float(loss)
+
+            if (i+1) % 5000 == 0:
+                torch.save(model, 'output/trained_model_'+str(epochs)+'_'+str(learning_rate)+'_'+str(i))
+
 
             del x, y, y_pred
-
-            if (j+1) % 1000 == 0:
-                train_loss = avg_loss / 1000
-                avg_loss = 0
-                val_loss = validate(model, val_feature_vec, val_label_vec)
-                scheduler.step(val_loss)
-                print('epoch %d: %d --- %f, %f' %(i, j, train_loss, val_loss))
-                writer.add_scalar('loss/train', train_loss, i)
-                writer.add_scalar('loss/validate', val_loss, i)
-
-            if (j+1) % 5000 == 0:
-                torch.save(model, 'output/trained_model_'+str(epochs)+'_'+str(learning_rate)+'_'+str(j))
-
-        train_accuracy = find_accuracy(model, train_feature_vec, train_label_vec)
-        val_accuracy = find_accuracy(model, val_feature_vec, val_label_vec)
-        print('----- validation accuracy: %f, train_accuracy: %f' %(val_accuracy, train_accuracy))
-        writer.add_scalar('accuracy/train', train_accuracy)
-        writer.add_scalar('accuracy/val', val_accuracy)
-
-    torch.save(model, 'output/final_model_'+str(epochs)+'_'+str(learning_rate))
-    return train_accuracy, val_accuracy
+    
+    torch.save(model, 'output/trained_model_'+str(epochs)+'_'+str(learning_rate))
+    return train_losses, val_losses    
